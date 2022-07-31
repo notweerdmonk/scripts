2a90e3bf413092b5ae3562e0d1dc92493332a116
notweerdmonk <wrdmnk@gmail.com>
2022-07-31 08:13:49 +0530

    wallpaper.sh: Add support for i3.

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,10 +1,17 @@
-EUID=$(id --real --user)
-PID=$(pgrep --euid $EUID gnome-session)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-n=$(shuf -i 1-$(ls /home/weerdmonk/Pictures | wc -l) -n 1);i=0
-for path in /home/weerdmonk/Pictures/*.jpg
+if [[ $(wmname) != "i3" ]]; then
+  MY_EUID=$(id --real --user)
+  GNOME_SESSION_PID=$(pgrep --euid $MY_EUID gnome-session | sed -n '1p')
+  export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$GNOME_SESSION_PID/environ|cut -d= -f2- | tr -d '\0')
+fi
+
+n=$(shuf -i 1-$(ls ${HOME}/Pictures | wc -l) -n 1);i=0
+for path in ${HOME}/Pictures/*.jpg
 do
   if [ $((i++)) -eq ${n} ]; then
-    gsettings set org.gnome.desktop.background picture-uri file:///${path};
+    if [[ $(wmname) == "i3" ]]; then
+      feh --bg-fil ${path}
+    else
+      gsettings set org.gnome.desktop.background picture-uri file:///${path};
+    fi
   fi
 done
