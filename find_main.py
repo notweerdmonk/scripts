8cbe77d329833e45edf9020d938b4e9902b51651
notweerdmonk <59772843+notweerdmonk@users.noreply.github.com>
2025-03-18 21:30:45 +0530

    Please don't ask

--- find_main.py
+++ find_main.py
@@ -1,4 +1,14 @@
-# Find address and/or step into the main function in an ELF stripped of symbols
+"""
+Copyright 2025 notweerdmonk
+
+I do not give anyone permissions to use this tool for any purpose. Don't use it.
+
+I’m not interested in changing this license. Please don’t ask.
+"""
+
+"""
+Find the main function in a program and more.
+"""
 
 import gdb
 import re
@@ -6,17 +16,34 @@ import re
 
 rax = ""
 rdx = ""
+rdi = ""
 rip = ""
 main_call_addr = ""
+main_addr = ""
 main_called = False
+
+
+def get_entry_point(start=False):
+    if start:
+        # Halt at _start of ld.so
+        gdb.execute("starti")
+
+    info_output = gdb.execute("info files", to_string=True)
+
+    for line in info_output.splitlines():
+        match = re.search(f"(Entry point: )(0x[a-zA-Z0-9]*)", line)
+        if match and len(match.groups()) == 2:
+            entry_point = match.group(2)
+            return entry_point
 
 
 def get_reg_value(register):
     try:
-        val = gdb.execute(f"info register ${register}", to_string=True).split()[1]
+        val = gdb.execute(f"output/x ${register}", to_string=True)
     except:
         return None
     return val
+
 
 def get_instr_addr(addr, l, instruction, op_hint=""):
     if not addr:
@@ -25,127 +52,237 @@ def get_instr_addr(addr, l, instruction, op_hint=""):
     disas_output = gdb.execute(f"disas {addr}, +{l}", to_string=True)
 
     for line in disas_output.splitlines():
-        if re.search(f"\s+call\s+.*{op_hint}", line):
-            line = line.lstrip("=>")[: line.find(":")].strip()
+        if re.search(f"\s+{instruction}\s+.*{op_hint}", line):
+            line = line.lstrip("=>")
+            line = line[:line.find(":")].strip()
             space_idx = line.find(" ")
             addr = line[:space_idx] if space_idx != -1 else line
-            if addr == -1:
-                return None
-            return addr
+            return None if len(addr) == 0 else addr
 
 
-def get_entry_point():
-    # Halt at _start of ld.so
-    gdb.execute("starti")
-
-    info_output = gdb.execute("info files", to_string=True)
-
-    for line in info_output.splitlines():
-        if re.search("\s+Entry point", line):
-            entry_point_line = line.strip()
-            entry_point_address = entry_point_line.split(":")[1].strip()
-            return entry_point_address
-
-
-def set_breakpoint(addr):
+def run_until(addr):
     if addr:
-        print(f"Setting breakpoint at addr: {addr}")
-        gdb.execute(f"break *{addr}")
+        print(f"Running program till {addr}")
+        if addr.startswith("0x"):
+            addr = "*" + addr
+        gdb.execute(f"until {addr}")
     else:
         print("Addr is None.")
+
+
+def set_breakpoint(addr, temp=False):
+    cmd = "tbreak" if temp else "break"
+    if addr:
+        print(f"Setting breakpoint at {addr}")
+        if addr.startswith("0x"):
+            addr = "*" + addr
+        gdb.execute(f"{cmd} {addr}")
+        return int(gdb.convenience_variable("bpnum"))
+    else:
+        print("Addr is None.")
+        return None
+
+
+def set_watchpoint(expr):
+    if expr:
+        print(f"Setting watchpoint for {expr}")
+        gdb.execute(f"watch {expr}")
+        return int(gdb.convenience_variable("bpnum"))
+    else:
+        print("Watch expression is None.")
+        return None
+
+
+def clear_breakpoint(num):
+    if isinstance(num, int):
+        gdb.execute(f"delete {num}")
+    else:
+        print("Invalid breakpoint number")
+
 
 def clear_all_breakpoints():
     gdb.execute("delete")
 
-def find_main(restart=False, step=False):
+
+# fast will override step
+def find_main(step=False, fast=False, restart=False, clean=False):
     global rax
     global rdx
+    global rdi
     global rip
     global main_call_addr
+    global main_addr
     global main_called
+
+    clean = True if clean else False
 
     if not restart:
         if step and not main_called and get_reg_value("rip") == main_call_addr:
             gdb.execute("stepi")
             main_called = True
-            return rax
+            main_addr = get_reg_value("rip")
+            return main_addr
 
         if len(main_call_addr) > 0:
-            return rax
+            return main_addr
+
+        if len(main_addr) > 0:
+            return main_addr
 
     clear_all_breakpoints()
     main_called = False
 
     # Set breakpoint at _start of ELF
-    set_breakpoint(get_entry_point())
+    set_breakpoint(get_entry_point(True), clean)
     gdb.execute("continue")
 
     # Next call is to __libc_start_main
     rip = get_reg_value("rip")
     call_addr = get_instr_addr(rip, 100, "call")
 
-    set_breakpoint(call_addr)
+    set_breakpoint(call_addr, clean)
     gdb.execute("continue")
 
-    gdb.execute("stepi")
+    if fast:
+        # Calling convention wise $rdi = &main
+        main_addr = rdi = get_reg_value("rdi")
+        set_breakpoint(rdi, clean)
+        gdb.execute("continue")
+        main_called = True
+        return main_addr
 
-    gdb.execute("break __libc_start_call_main")
-    gdb.execute("continue")
+    else:
+        gdb.execute("stepi")
 
-    # Track argv value as it gets loaded into rsi as argument to find the call
-    # to main
-    rdx = get_reg_value("rdx")
+        set_breakpoint("__libc_start_call_main", clean)
+        gdb.execute("continue")
 
-    gdb.execute(f"watch $rsi == {rdx}")
-    gdb.execute("continue")
+        # Track argv value as it gets loaded into rsi as argument to find the call
+        # to main
+        rdx = get_reg_value("rdx")
 
-    # Address of main is loaded into rax
-    # Instruction looks like:
-    # 0x0000xxxxxxxxxxxx:  call    rax
-    rip = get_reg_value("rip")
-    call_addr = get_instr_addr(rip, 20, "call")
+        watchpoint_num = set_watchpoint(f"$rsi == {rdx}")
 
-    set_breakpoint(call_addr)
-    gdb.execute("continue")
+        gdb.execute("continue")
 
-    main_call_addr = get_reg_value("rip")
+        if clean:
+            clear_breakpoint(watchpoint_num)
 
-    rax = get_reg_value("rax")
+        # Address of main is loaded into rax
+        # Instruction looks like:
+        # 0x0000xxxxxxxxxxxx:  call    rax
+        rip = get_reg_value("rip")
+        call_addr = get_instr_addr(rip, 20, "call")
+
+        set_breakpoint(call_addr, clean)
+        gdb.execute("continue")
+
+        main_call_addr = rip = get_reg_value("rip")
+
+        main_addr = rax = get_reg_value("rax")
 
     if step:
         gdb.execute("stepi")
         main_called = True
 
-    return rax
+    return main_addr
 
 
-class RegisterEntryPointCommand(gdb.Command):
+class register_get_entry_point_command(gdb.Command):
+    """
+    Get the entry point of the program. The program needs to be running else
+    the offset in the program file will be provided.
+
+    Usage:
+        get_entry_point [start]
+
+    Options:
+        start       Start the program if any argument is given.
+    """
+
     def __init__(self):
-        super(RegisterEntryPointCommand, self).__init__(
+        super(register_get_entry_point_command, self).__init__(
             "get_entry_point", gdb.COMMAND_USER
         )
 
     def invoke(self, arg, from_tty):
-        entry_point = get_entry_point()
+        arg = True if arg else False
+        entry_point = get_entry_point(arg)
         if entry_point:
             print(f"Entry point address: {entry_point}")
         else:
             print("Entry point not found.")
 
 
-class RegisterSetBreakpointCommand(gdb.Command):
+class register_run_until_command(gdb.Command):
+    """
+    Run the program till given address within the current frame.
+
+    Usage:
+        run_until <address>
+    """
+
     def __init__(self):
-        super(RegisterSetBreakpointCommand, self).__init__(
+        super(register_run_until_command, self).__init__(
+            "run_until", gdb.COMMAND_USER
+        )
+
+    def invoke(self, arg, from_tty):
+        run_until(arg)
+
+
+class register_set_breakpoint_command(gdb.Command):
+    """
+    Set a breakpoint at the given address.
+
+    Usage:
+        set_breakpoint <address> [temporary]
+
+    Options:
+        temporary       Set a temporary breakpoint if any argument is given.
+    """
+
+    def __init__(self):
+        super(register_set_breakpoint_command, self).__init__(
             "set_breakpoint", gdb.COMMAND_USER
         )
 
     def invoke(self, arg, from_tty):
-        set_breakpoint(arg)
+        args = [a.rstrip(",") for a in arg.split()]
+        if len(args) > 1:
+            set_breakpoint(args[0], args[1])
+        else:
+            set_breakpoint(args[0])
 
 
-class RegisterGetInstructionAddrCommand(gdb.Command):
+class register_set_watchpoint_command(gdb.Command):
+    """
+    Set a watchpoint for the given expression.
+
+    Usage:
+        set_watchpoint <expression>
+    """
+
     def __init__(self):
-        super(RegisterGetInstructionAddrCommand, self).__init__(
+        super(register_set_watchpoint_command, self).__init__(
+            "set_watchpoint", gdb.COMMAND_USER
+        )
+
+    def invoke(self, arg, from_tty):
+        set_watchpoint(arg)
+
+
+class register_get_instruction_addr_command(gdb.Command):
+    """
+    Get the address of next occurence of given instruction matching given hints
+    for operands starting from given address till given number of bytes.
+
+    Usage:
+        get_instr_addr <address> <length> <instruction> [hint]
+    """
+
+    def __init__(self):
+        super(register_get_instruction_addr_command, self).__init__(
             "get_instr_addr", gdb.COMMAND_USER
         )
 
@@ -158,34 +295,76 @@ class RegisterGetInstructionAddrCommand(gdb.Command):
         )
 
 
-class RegisterFindMainCommand(gdb.Command):
+class register_find_main_command(gdb.Command):
+    """
+    Find the main function of the program and halt before the call.
+
+    Usage:
+        find_main [fast] [restart] [clean]
+
+    Options:
+        fast        Use faster method skipping intermittent breakpoints.
+        restart     Restart the procedure.
+        clean       Use temporary breakpoints and clear watchpoints.
+    """
+
     def __init__(self):
-        super(RegisterFindMainCommand, self).__init__(
+        super(register_find_main_command, self).__init__(
             "find_main", gdb.COMMAND_USER
         )
 
     def invoke(self, arg, from_tty):
+        args = [a.rstrip(",") for a in arg.split()]
+        clean = False
+        if "c" in args or "clean" in args:
+            clean = True
         restart = False
-        if arg == "r" or arg == "restart":
+        if "r" in args or "restart" in args:
             restart = True
-        print(f"Address of main function is: {find_main(restart, False)}")
+        fast = False
+        if "f" in args or "fast" in args:
+            fast = True
+        print(f"Address of main function is: {find_main(False, fast, restart, clean)}")
+        if fast:
+            print("Stepped into main function")
 
 
-class RegisterStepMainCommand(gdb.Command):
+class register_step_main_command(gdb.Command):
+    """
+    Step inside the main function of the program and halt before the call.
+
+    Usage:
+        step_main [fast] [restart] [clean]
+
+    Options:
+        fast        Use faster method skipping intermittent breakpoints.
+        restart     Restart the procedure.
+        clean       Use temporary breakpoints and clear watchpoints.
+    """
     def __init__(self):
-        super(RegisterStepMainCommand, self).__init__(
+        super(register_step_main_command, self).__init__(
             "step_main", gdb.COMMAND_USER
         )
 
     def invoke(self, arg, from_tty):
+        args = [a.rstrip(",") for a in arg.split()]
+        clean = False
+        if "c" in args or "clean" in args:
+            clean = True
         restart = False
-        if arg == "r" or arg == "restart":
+        if "r" in args or "restart" in args:
             restart = True
-        print(f"Address of main function is: {find_main(restart, True)}")
+        fast = False
+        if "f" in args or "fast" in args:
+            fast = True
+        print(f"Address of main function is: {find_main(True, fast, restart, clean)}")
+        print("Stepped into main function")
 
 
-RegisterEntryPointCommand()
-RegisterSetBreakpointCommand()
-RegisterGetInstructionAddrCommand()
-RegisterFindMainCommand()
-RegisterStepMainCommand()
+register_get_entry_point_command()
+register_get_instruction_addr_command()
+register_run_until_command()
+register_set_breakpoint_command()
+register_set_watchpoint_command()
+register_find_main_command()
+register_step_main_command()
